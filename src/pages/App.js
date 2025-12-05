6b9d6063e5cc347ab67eeb8b37f2b73eef5cb9a7
GustavoDiniz17 <gustavodinizdemorais@gmail.com>
2025-12-05 09:57:27 -0300

    Preparação do Projeto

---
+++ src/pages/App.js
@@ -0,0 +1,13 @@
+
+import gitLogo from '../assets/github.svg'
+
+import {Container} from './styles'
+function App() {
+  return (
+    <Container>
+      <img src={gitLogo} width={72} height={72} alt='githu-logo'/>
+    </Container>
+  );
+}
+
+export default App;
