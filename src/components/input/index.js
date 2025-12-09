69142f3242d2f72a2d2affbfad0b97d6060c7b50
GustavoDiniz17 <gustavodinizdemorais@gmail.com>
2025-12-09 08:27:45 -0300

    Criando Componente de Input

---
+++ src/components/input/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+import {InputConainer} from './styles'
+function Input() {
+  return (
+    <InputConainer>
+      <input />
+    </InputConainer>
+  )
+}
+
+export default Input
--- src/pages/App.js
+++ src/pages/App.js
@@ -1,11 +1,13 @@
 
 import gitLogo from '../assets/github.svg'
+import Input from '../components/input';
 
 import {Container} from './styles'
 function App() {
   return (
     <Container>
       <img src={gitLogo} width={72} height={72} alt='githu-logo'/>
+      <Input />
     </Container>
   );
 }
